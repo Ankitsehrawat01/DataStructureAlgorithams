ba1f37fa7b4edb5103d5e1a591bbc6cc2314ab59
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-09-06 04:07:59 +0530

    Sort using Merge sort

--- Program.cs
+++ Program.cs
@@ -13,7 +13,8 @@ namespace DataStructureAlgorithams
                 Console.WriteLine("1) Permutation of string Using Recursion\n" +
                                   "2) BinarySearch of a word from Text File\n" +
                                   "3) Insertion Sort\n" +
-                                  "4) Bubble Sorting\n");
+                                  "4) Bubble Sorting\n" +
+                                  "5) Merge Sort\n");
                 Console.WriteLine("Enter Your Option");
                 int num = Convert.ToInt32(Console.ReadLine());
                 switch (num)
@@ -33,6 +34,30 @@ namespace DataStructureAlgorithams
                     case 4:
                         BubbleSort bubblesortobj = new BubbleSort();
                         bubblesortobj.bubbleSort();
+                        break;
+                    case 5:
+                        MergeSort mergetobj = new MergeSort();
+                        Console.Write("\nProgram for sorting a numeric array using Merge Sorting");
+                        Console.Write("\n\nEnter number of elements: ");
+                        int max = Convert.ToInt32(Console.ReadLine());
+                        int[] numbers = new int[max];
+                        for (int i = 0; i < max; i++)
+                        {
+                            Console.Write("\nEnter [" + (i + 1).ToString() + "] element: ");
+                            numbers[i] = Convert.ToInt32(Console.ReadLine());
+                        }
+                        Console.Write("Input int array : ");
+                        Console.Write("\n");
+                        for (int k = 0; k < max; k++)
+                        {
+                            Console.Write(numbers[k] + " ");
+                            Console.Write("\n");
+                        }
+                        Console.WriteLine("MergeSort By Recursive Method");
+                        mergetobj.SortMerge(numbers, 0, max - 1);
+                        for (int i = 0; i < max; i++)
+                            Console.WriteLine(numbers[i]);
+                        Console.ReadLine();
                         break;
                 }
             }
